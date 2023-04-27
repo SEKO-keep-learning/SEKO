041dce2ae5242c278a2dd3354a839592577e2bbc
Chirag-22-10 <117558849+Chirag-22-10@users.noreply.github.com>
2023-04-27 20:57:43 +0530

    Login and Register Backend

--- app/src/main/java/com/example/seko/MainActivity.kt
+++ app/src/main/java/com/example/seko/MainActivity.kt
@@ -1,11 +1,89 @@
 package com.example.seko
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.seko.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    private var email : EditText? = null
+    private var pass : EditText? = null
+    private val db = Firebase.firestore
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+        val btnLogin : TextView = findViewById(R.id.btnLogin)
+        email = findViewById(R.id.emailSignIn)
+        pass = findViewById(R.id.passSignIn)
+        btnLogin.setOnClickListener{
+            signIn()
+        }
+
+    }
+    private fun signIn(){
+        val et_email : String = email?.text.toString().trim{ it <= ' '}
+        val et_pass : String = pass?.text.toString().trim{ it <= ' '}
+
+
+
+        if(validate(et_email, et_pass)){
+            auth.signInWithEmailAndPassword(et_email, et_pass)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("Sign in", "signInWithEmail:success")
+                        val user = auth.currentUser
+
+                        Toast.makeText(this@MainActivity, "Logged in", Toast.LENGTH_LONG).show()
+
+                        // updateUI(user)
+                        // readData()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("Sign in", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        // updateUI(null)
+                    }
+                }
+        }
+
+    }
+
+    private fun validate(et_email : String, et_pass : String):Boolean{
+        return when{
+            TextUtils.isEmpty(et_email) -> {
+                showErrorSnackBar("Please enter proper username")
+                false
+            }
+            TextUtils.isEmpty(et_pass) -> {
+                showErrorSnackBar("Please enter proper password")
+                false
+            }
+            else -> {
+                true
+            }
+        }
+
     }
 }
+
+
+
+
+
--- app/src/main/java/com/example/seko/SignupActivity.kt
+++ app/src/main/java/com/example/seko/SignupActivity.kt
@@ -1,11 +1,51 @@
 package com.example.seko
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.seko.databinding.ActivityMainBinding
+import com.example.seko.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnSignUp.setOnClickListener {
+            val name = binding.nameRegister.text.toString()
+            val email = binding.emailRegister.text.toString()
+            val pass = binding.passwordRegister.text.toString()
+
+            if (name.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty()) {
+                firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_LONG).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields are not allowed", Toast.LENGTH_LONG).show()
+            }
+
+        }
+        binding.btnSignin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
