3fc5501704cef7650dc82f82bf2412af2bd29ddb
risha <rishabhcoder01@gmail.com>
2023-06-19 11:23:17 +0530

    Rishi Slash Screen Done

---
+++ app/src/main/java/com/example/seko/spalsh_screen.kt
@@ -0,0 +1,11 @@
+package com.example.seko
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class spalsh_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spalsh_screen)
+    }
+}
